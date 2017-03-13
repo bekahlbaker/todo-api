a9042ea1389813f86620218790e0dcea42fb3bda
bekahlbaker <bekahlbaker@gmail.com>
2017-03-13 16:13:09 -0500

    Added /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	item: 'Give cats bath',
+	completed: false
+},
+{
+	id: 2,
+	item: 'Wash the dishes',
+	completed: false
+}, 
+{
+	id: 3,
+	item: 'Clean the bathrooms',
+	completed: true
+}];
 
 //GET
 app.get('/', function(req, res) {
@@ -8,6 +23,37 @@ app.get('/', function(req, res) {
 	res.send('Todo API root');
 });
 
+//GET /todos
+app.get('/todos', function(req, res) {
+	console.log("GET todos from server");
+	res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+
+});
+
+
 app.listen(PORT, function () {
 	console.log("Listening on port" + PORT);
 });
+
+
+
+
+
