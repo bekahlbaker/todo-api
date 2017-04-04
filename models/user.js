6166d530a56992e977dacc53bb439f772e2d790b
bekahlbaker <bekahlbaker@gmail.com>
2017-04-04 15:48:22 -0500

    Remove auth from publicJson

--- models/user.js
+++ models/user.js
@@ -90,7 +90,7 @@ module.exports = function(sequelize, DataTypes) {
 		instanceMethods: {
 			toPublicJSON: function() {
 				var json = this.toJSON();
-				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt', 'Auth');
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			},
 			generateToken: function(type) {
 				if (!_.isString(type)) {
